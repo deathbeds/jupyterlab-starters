451e890d6a7e292e552b4d487213753949ba01d7
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-12-13 21:00:55 -0500

    actually read version from file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 __version__ = re.findall(
     r"""__version__ = "(.*?)"$""",
-    Path(__file__).parent / "src" / "jupyter_starters" / "_version"
+    (Path(__file__).parent / "src" / "jupyter_starters" / "_version.py").read_text()
 )[0]
 
 __import__("setuptools").setup(version=__version__)
