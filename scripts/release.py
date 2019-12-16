a22f4d5752ff8651ff29630d0b2146b0d7995fef
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-12-15 21:40:24 -0500

    clean up some release stuff

--- scripts/release.py
+++ scripts/release.py
@@ -7,13 +7,13 @@ from subprocess import check_call
 def release(registry=None):
     """ do a (test) release
     """
-    registry = registry or os.environ.get("PYPI_REGISTRY", "pypitest")
+    registry = registry or os.environ.get(
+        "PYPI_REGISTRY", "https://test.pypi.org/legacy/"
+    )
     setup = ["python", "setup.py"]
-    check_call([*setup, "register", "-r", registry])
     check_call([*setup, "sdist"])
     check_call([*setup, "bdist_wheel"])
-    check_call([*setup, "sdist", "upload", "-r", registry])
-    check_call([*setup, "bdist_wheel", "upload", "-r", registry])
+    check_call(["twine", "upload", "--repository-url", registry, "dist/*"])
 
 
 if __name__ == "__main__":
