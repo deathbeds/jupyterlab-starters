74fcf6aec8fd4c61afce7f474f9ee8eab41c87a6
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-12-16 02:45:41 -0500

    kill some dead code

--- src/jupyter_starters/trait_types.py
+++ src/jupyter_starters/trait_types.py
@@ -25,24 +25,3 @@ class Schema(traitlets.Any):
         except JsonSchemaException as err:
             raise traitlets.TraitError(f"""schema errors: {err}""")
         return value
-
-
-class LoadableCallable(traitlets.TraitType):
-    """A trait which (maybe) loads a callable."""
-
-    info_text = "a loadable callable"
-
-    def validate(self, obj, value):
-        """ is it loadable? can it be made callable?
-        """
-        if isinstance(value, str):
-            try:
-                value = traitlets.import_item(value)
-            except Exception:
-                self.error(obj, value)
-
-        if six.callable(value):
-            return value
-
-        self.error(obj, value)
-        return None
