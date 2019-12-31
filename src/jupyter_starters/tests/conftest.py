5bf39f1462dc6790bbdeffea291dbaeda0b69246
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-12-30 22:21:18 -0500

    test some py_src stuff

--- src/jupyter_starters/tests/conftest.py
+++ src/jupyter_starters/tests/conftest.py
@@ -10,3 +10,18 @@ def starter_manager():
     """ an orphaned starter
     """
     return StarterManager()
+
+
+@pytest.fixture
+def example_project(tmp_path):
+    """ a minimal project
+    """
+    my_module = tmp_path / "my_module"
+    starter_content = my_module / "starter_content"
+    starter_content.mkdir(parents=True)
+
+    (tmp_path / "README.md").write_text("# My Module\n")
+    (my_module / "__init__.py").write_text("__version__ = '0.0.0\n")
+    (starter_content / "example.txt").write_text("123")
+
+    return tmp_path
--- src/jupyter_starters/tests/test_src.py
+++ src/jupyter_starters/tests/test_src.py
@@ -1,5 +1,8 @@
 """ test the sanity of the src/py_src behavior
 """
+# pylint: disable=too-many-arguments,bad-continuation
+from pathlib import Path
+
 import pytest
 
 
@@ -7,7 +10,7 @@ import pytest
     "name,starter",
     [
         ["doesn't exist", {"src": "doesnt-exist"}],
-        ["not inside", {"py_src": "jupyter_starters", "src": "../"}],
+        ["not inside", {"py_src": "jupyter_starters", "src": ".."}],
         ["not importable", {"py_src": "not_a_module", "src": "doesnt-matter"}],
     ],
 )
@@ -15,3 +18,26 @@ def test_bad_src(starter_manager, name, starter):
     """ these are bad sources
     """
     assert starter_manager.resolve_src(starter) is None, f"{name} should have been None"
+
+
+@pytest.mark.parametrize(
+    "name,starter,py_path",
+    [
+        ["local file", {"src": "README.md"}, False],
+        ["local directory", {"src": "my_module/starter_content"}, False],
+        ["py file", {"src": "__init__.py", "py_src": "my_module"}, True],
+        ["py directory", {"src": "starter_content", "py_src": "my_module"}, True],
+    ],
+)
+def test_good_src(
+    name, starter, py_path, monkeypatch, example_project, starter_manager
+):
+    """ these are good sources
+    """
+    if py_path:
+        monkeypatch.syspath_prepend(example_project)
+    else:
+        monkeypatch.chdir(example_project)
+
+    src = starter_manager.resolve_src(starter)
+    assert src and isinstance(src, Path) and src.exists(), f"{name} {src} is bad"
