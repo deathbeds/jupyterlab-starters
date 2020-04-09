971b5472410214c0da597ba13367a2e1f81c62b5
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-04-08 22:43:40 -0400

    fix broad exception in lint

--- scripts/docs.py
+++ scripts/docs.py
@@ -12,9 +12,9 @@ import jinja2
 HAS_PYTEST = False
 
 try:
-    __import__("pytest_check_links")
+    import pytest_check_links
     HAS_PYTEST = True
-except Exception as err:
+except ImportError as err:
     print("pytest_check_links not available, skipping link check", err)
 
 
