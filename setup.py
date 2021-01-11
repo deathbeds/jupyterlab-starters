606832a02ac135da6f45595d6f7610ad856181b5
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-01-11 11:49:52 -0500

    more work on data files

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ from pathlib import Path
 import setuptools
 
 HERE = Path(__file__).parent
-EXT = HERE / "src" / "jupyter_starters" / "labextension"
+SRC = HERE / "src" / "jupyter_starters"
+ETC = SRC / "etc"
+CONF = ETC / "jupyter-starters-serverextension.json"
+EXT = SRC / "labextension"
 EXT_NAME = "@deathbeds/jupyterlab-starters"
 
 EXT_FILES = {}
@@ -16,11 +19,11 @@ SHARE = f"share/jupyter/labextensions/{EXT_NAME}"
 
 for ext_path in [EXT] + [d for d in EXT.rglob("*") if d.is_dir()]:
     if ext_path == EXT:
-        target = str(SHARE)
+        target = SHARE
     else:
         target = f"{SHARE}/{ext_path.relative_to(EXT)}"
     EXT_FILES[target] = [
-        str(p.relative_to(HERE).as_posix())
+        p.relative_to(HERE).as_posix()
         for p in ext_path.glob("*")
         if not p.is_dir()
     ]
@@ -31,27 +34,18 @@ assert (
     len([p for p in ALL_FILES if "remoteEntry" in str(p)]) == 1
 ), "expected _exactly one_ remoteEntry.*.js"
 
-EXT_FILES[str(SHARE)] += ["src/jupyter_starters/etc/install.json"]
+EXT_FILES[SHARE] += [ETC / "install.json"]
 
+# TODO: replace with version file or package.json
+VERSION_RE = r"""__version__ = "(.*?)"$"""
 
 SETUP_ARGS = dict(
-    version=re.findall(
-        r"""__version__ = "(.*?)"$""",
-        (Path(__file__).parent / "src" / "jupyter_starters" / "_version.py").read_text()
-    )[0],
+    version=re.findall(VERSION_RE, (SRC / "_version.py").read_text())[0],
     data_files=[
-
-        (
-            "etc/jupyter/jupyter_server_config.d",
-            ["src/jupyter_starters/etc/jupyter-starters-serverextension.json"],
-        ),
-        (
-            "etc/jupyter/jupyter_notebook_config.d",
-            ["src/jupyter_starters/etc/jupyter-starters-serverextension.json"],
-        ),
-        *[(k, v) for k, v in EXT_FILES.items()],
+        ("etc/jupyter/jupyter_server_config.d", [str(CONF)]),
+        ("etc/jupyter/jupyter_notebook_config.d", [str(CONF)]),
+        *[(str(k), list(map(str, v))) for k, v in EXT_FILES.items()],
     ],
-
 )
 
 if __name__ == "__main__":
