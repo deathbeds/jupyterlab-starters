6f96ea9cd66f0865d2a6a54206d948c715d539c3
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-01-31 15:47:30 -0600

    more sourcemaps for debugging

--- packages/jupyterlab-starters/webpack.config.js
+++ packages/jupyterlab-starters/webpack.config.js
@@ -1,20 +1,32 @@
 module.exports = {
-    output: {
-      clean: true,
-    },
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: process.env.WITH_JS_COV
-            ? ['@ephesoft/webpack.istanbul.loader']
-            : ['source-map-loader'],
-        },
-        {
-          test: /\.html/,
-          type: 'asset/resource',
-        },
-      ],
-    },
-  };
+  output: {
+    clean: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: process.env.WITH_JS_COV
+          ? ['@ephesoft/webpack.istanbul.loader']
+          : [
+              {
+                loader: 'source-map-loader',
+                options: {
+                  filterSourceMappingUrl: (url, resourcePath) => {
+                    if (resourcePath.includes('@jupyterlab/rendermime')) {
+                      return false;
+                    }
+                    return true;
+                  },
+                },
+              },
+            ],
+      },
+      {
+        test: /\.html$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
